5a82227490b6f7cfa606db9f472cb488777e74fb
imalka111 <bskprasad76@gmail.com>
2024-12-20 23:20:09 +0530

    ☘ 𝗛𝗔𝗡𝗦𝗔𝗠𝗔𝗟 𝗠𝗗 〽️

--- nonbutton.js
+++ nonbutton.js
@@ -1,25 +1,12 @@
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, getsize} = require('./lib/functions')
 
-function btregex(dta) {
-    const btn = '/({11})/'
-    return h2k(btn.test(dta))
-}
-buttonMessage = async (gg, newmg) => {
-    newmg.forEach((section, sectionIndex) => {
-        let number = 1
-        result += `\n*[${mainNumber}] ${section.title}*\n`;
-        reply('*Select number' + number++ + sectionIndex + result)
-        sectionIndex.push(Json(gg))
-})
-}
+/**
+██╗░░██╗░█████╗░███╗░░██╗░██████╗░█████╗░███╗░░░███╗░█████╗░██╗░░░░░░░░░░░███╗░░░███╗██████╗░
+██║░░██║██╔══██╗████╗░██║██╔════╝██╔══██╗████╗░████║██╔══██╗██║░░░░░░░░░░░████╗░████║██╔══██╗
+███████║███████║██╔██╗██║╚█████╗░███████║██╔████╔██║███████║██║░░░░░█████╗██╔████╔██║██║░░██║
+██╔══██║██╔══██║██║╚████║░╚═══██╗██╔══██║██║╚██╔╝██║██╔══██║██║░░░░░╚════╝██║╚██╔╝██║██║░░██║
+██║░░██║██║░░██║██║░╚███║██████╔╝██║░░██║██║░╚═╝░██║██║░░██║███████╗░░░░░░██║░╚═╝░██║██████╔╝
+╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░╚═╝╚══════╝░░░░░░╚═╝░░░░░╚═╝╚═════╝░
+🇲​​​​​🇦​​​​​🇩​​​​​🇪​​​​​ 🇧​​​​​🇾​​​​​ 🇮​​​​​🇲​​​​​🇦​​​​​🇱​​​​​🇰​​​​​🇦​​​​​ 🇭​​​​​🇦​​​​​🇳​​​​​🇸​​​​​🇦​​​​​🇲​​​​​🇦​​​​​🇱​​​​​*/
 
-listMessage = async (gg, newmg) => {
-    newmg.forEach((section, sectionIndex) => {
-        let number = 1.0
-        result += `\n*[${mainNumber}] ${section.title}*\n`;
-        reply('*Select number' + number++ + sectionIndex + result)
-        sectionIndex.push(Json(gg))
-})
-}
 
-module.export = {listMessage, buttonMessage, btregex}
+function _0x5421(_0x1b7620,_0x5b926e){const _0x506375=_0x5063();return _0x5421=function(_0x5421b1,_0x4d1ad1){_0x5421b1=_0x5421b1-0x9b;let _0x255173=_0x506375[_0x5421b1];return _0x255173;},_0x5421(_0x1b7620,_0x5b926e);}function _0x5063(){const _0xf8c249=['./lib/functions','\x0a*[','2687862DtYonR','265076yionLb','*Select\x20number','title','3658232pMqTVf','9NGlIPD','export','push','forEach','1693480VwqAvT','3289995DrIyXs','91hPsjBy','/({11})/','54354PGzCAB','test','459632eqCHCC'];_0x5063=function(){return _0xf8c249;};return _0x5063();}const _0x3566c7=_0x5421;(function(_0x5d3ff5,_0x31c560){const _0xc13af3=_0x5421,_0x53c7df=_0x5d3ff5();while(!![]){try{const _0x792748=-parseInt(_0xc13af3(0x9d))/0x1+-parseInt(_0xc13af3(0xa5))/0x2+parseInt(_0xc13af3(0x9c))/0x3+-parseInt(_0xc13af3(0xab))/0x4+parseInt(_0xc13af3(0xa6))/0x5+-parseInt(_0xc13af3(0xa9))/0x6*(-parseInt(_0xc13af3(0xa7))/0x7)+-parseInt(_0xc13af3(0xa0))/0x8*(-parseInt(_0xc13af3(0xa1))/0x9);if(_0x792748===_0x31c560)break;else _0x53c7df['push'](_0x53c7df['shift']());}catch(_0x2a8231){_0x53c7df['push'](_0x53c7df['shift']());}}}(_0x5063,0xdc483));const {getBuffer,getGroupAdmins,getRandom,h2k,isUrl,Json,runtime,sleep,fetchJson,getsize}=require(_0x3566c7(0xac));function btregex(_0x490c27){const _0x2dea7a=_0x3566c7,_0x2be6f4=_0x2dea7a(0xa8);return h2k(_0x2be6f4[_0x2dea7a(0xaa)](_0x490c27));}buttonMessage=async(_0x14d9a6,_0x126ebd)=>{const _0x52e06c=_0x3566c7;_0x126ebd[_0x52e06c(0xa4)]((_0x50f685,_0x33a63f)=>{const _0x4a5dba=_0x52e06c;let _0x253294=0x1;result+=_0x4a5dba(0x9b)+mainNumber+']\x20'+_0x50f685['title']+'*\x0a',reply(_0x4a5dba(0x9e)+_0x253294++ +_0x33a63f+result),_0x33a63f['push'](Json(_0x14d9a6));});},listMessage=async(_0x3ce9c9,_0x59fb2c)=>{const _0x1898fc=_0x3566c7;_0x59fb2c[_0x1898fc(0xa4)]((_0x2d9dd1,_0x73f9d9)=>{const _0x16e99e=_0x1898fc;let _0x64f192=0x1;result+='\x0a*['+mainNumber+']\x20'+_0x2d9dd1[_0x16e99e(0x9f)]+'*\x0a',reply('*Select\x20number'+_0x64f192++ +_0x73f9d9+result),_0x73f9d9[_0x16e99e(0xa3)](Json(_0x3ce9c9));});},module[_0x3566c7(0xa2)]={'listMessage':listMessage,'buttonMessage':buttonMessage,'btregex':btregex};
