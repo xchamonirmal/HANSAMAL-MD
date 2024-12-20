3e758efa56e5e48d670dce08138db3757056bfa7
imalka111 <bskprasad76@gmail.com>
2024-12-21 00:05:45 +0530

    ☘ 𝗛𝗔𝗡𝗦𝗔𝗠𝗔𝗟 𝗠𝗗 〽️

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: 'YOUR SESSION ID',
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (ghp_without)' : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
 };
