ac254594d58a45077eab726525680471b52f1f3d
xchamonirmal <herokubot458@gmail.com>
2024-12-22 10:46:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 SESSION_ID: 'YOUR SESSION ID',
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'F5ddKGWr7D2dBbEAG1PG69Z4Dzyeo02iToij' : process.env.GITHUB_AUTH_TOKEN,
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'xchamonirmal' : process.env.GITHUB_USER_NAME,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94743430452' : process.env.OWNER_NUMBER
 };
