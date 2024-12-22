d48574b94336d710757243b142899d53b787a1e0
xchamonirmal <herokubot458@gmail.com>
2024-12-22 10:49:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
+SESSION_ID: 'HANSAMAL-MD=nZkQzDQA#2C75-KxSAeQd9PHrhazxPGnO0g5NHes6TjHCmt_Xgz8',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'F5ddKGWr7D2dBbEAG1PG69Z4Dzyeo02iToij' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'xchamonirmal' : process.env.GITHUB_USER_NAME,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94743430452' : process.env.OWNER_NUMBER
